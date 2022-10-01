0370b5b4a9057e590bda4521d003c0a2e5209e79
TonMoshi <chingowamen@hotmail.com>
2022-10-01 18:00:05 +0200

    changed routes in commands

--- src/model/commands.ts
+++ src/model/commands.ts
@@ -2,7 +2,7 @@ import config from "../util/config";
 
 export const commands = {
   createFolderHierarchy: (serverName: String) =>
-    `mkdir -p ~/${config.MAIN_FOLDER}/${serverName}/Server ~/${config.MAIN_FOLDER}/${serverName}/Logs ~/${config.MAIN_FOLDER}/${serverName}/Scripts`,
+    `mkdir -p ${config.MAIN_FOLDER}/${serverName}/Server ${config.MAIN_FOLDER}/${serverName}/Logs ~/${config.MAIN_FOLDER}/${serverName}/Scripts`,
 
-  generateBaseTree: `tree ~/${config.MAIN_FOLDER}`,
+  generateBaseTree: `tree ${config.MAIN_FOLDER}`,
 };
